2d2e157262cd8ba93a1d556a5d0bf80b6c0e1012
Brian De Maio <bdemaio1@gmail.com>
2019-02-16 15:31:22 -0500

    Checking if redis implementation on mongoose exec prototype is returning cached data

--- services/cache.js
+++ services/cache.js
@@ -1,7 +1,27 @@
 const mongoose = require("mongoose");
 const exec = mongoose.Query.prototype.exec;
+const redis = require("redis");
+const util = require("util");
 
-mongoose.Query.prototype.exec = function() {
-  console.log("I'm going to run a query");
-  return exec.apply(this, arguments);
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+  console.log(key);
+  //Check if we have value for the key in redis
+  const cacheValue = await client.get(key);
+
+  if (cacheValue) {
+    console.log(cacheValue);
+  }
+
+  //Get mongo db call and apply to redis
+  const result = await exec.apply(this, arguments);
+  console.log(result);
 };
