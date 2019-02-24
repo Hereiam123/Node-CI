d48e5c477f2879e6c79e3c75f4dd4e977c4edc93
Brian De Maio <bdemaio1@gmail.com>
2019-02-24 11:54:24 -0500

    Able to run test with cookie authentication, without needing to go through Google Oauth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -9,7 +9,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  //await browser.close();
 });
 
 test("The header has the correct text", async () => {
@@ -21,4 +21,27 @@ test("Click on Google Login link and goes to Google Oauth", async () => {
   await page.click(".right a");
   const pageUrl = page.url();
   expect(pageUrl).toMatch(/accounts\.google\.com/);
+});
+
+test.only("When signed in, shows logged out button", async () => {
+  const id = "5c677a7d56e7fd228c098814";
+
+  const Buffer = require("safe-buffer").Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionString);
+
+  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
 });
