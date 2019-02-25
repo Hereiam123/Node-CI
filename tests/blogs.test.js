8a10f4f523a6d8d2c1b0f1553eb137bc7fe5da28
hereiam123 <bdemaio1@gmail.com>
2019-02-24 21:05:07 -0500

    Editing valid input test to prevent crash on Mongo call

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -21,7 +21,29 @@ describe("When logged in", async () => {
     expect(text).toEqual("Blog Title");
   });
 
-  describe("using invlaid inputs", async () => {
+  describe("using valid inputs", async () => {
+    beforeEach(async () => {
+      await page.type(".title input", "Hello");
+      await page.type(".content input", "Hello");
+      await page.click("form button.btn-flat.right");
+    });
+
+    test("Submitting takes user to review page", async () => {
+      const headerText = await page.getContentsOf("form h5");
+      expect(headerText).toEqual("Please confirm your entries");
+    });
+
+    test("Submitting adds blog to index page", async () => {
+      await page.click("button.green.btn-flat");
+      await page.waitFor(".card");
+      const cardTitle = await page.getContentsOf(".card-content .card-title");
+      const cardParagraph = await page.getContentsOf(".card-content p");
+      expect(cardTitle).toEqual("Hello");
+      expect(cardParagraph).toEqual("Hello");
+    });
+  });
+
+  describe("using invalid inputs", async () => {
     beforeEach(async () => {
       await page.click("form button.btn-flat.right");
     });
