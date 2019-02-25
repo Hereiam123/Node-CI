a156ef2cefb8c5abe4275d35a5da4fe3e75fc2a5
hereiam123 <bdemaio1@gmail.com>
2019-02-24 20:14:31 -0500

    Checking for invalid inputs

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -16,8 +16,20 @@ describe("When logged in", async () => {
     await page.click("a.btn-floating");
   });
 
-  test("We can see blog creation form", async () => {
+  test("we can see blog creation form", async () => {
     const text = await page.getContentsOf("form .title label");
     expect(text).toEqual("Blog Title");
+  });
+
+  describe("using invlaid inputs", async () => {
+    beforeEach(async () => {
+      await page.click("form button.btn-flat.right");
+    });
+    test("form shows error message", async () => {
+      const titleError = await page.getContentsOf(".title .red-text");
+      const contentError = await page.getContentsOf(".content .red-text");
+      expect(titleError).toEqual("You must provide a value");
+      expect(contentError).toEqual("You must provide a value");
+    });
   });
 });
