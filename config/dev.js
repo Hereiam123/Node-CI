df088671f2ce0dd30f2d7505e4c97303235b8922
hereiam123 <bdemaio1@gmail.com>
2019-02-26 23:28:10 -0500

    Fixed untracked files

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    "668442515260-atdbgi0t8q0pgq1tqs2p5t85qkiic7jp.apps.googleusercontent.com",
-  googleClientSecret: "g6XMYP0O1IBkZ8XGe-U0UyML",
-  mongoURI:
-    "mongodb://hereiam123:youRay123@cluster0-shard-00-00-erkiu.mongodb.net:27017,cluster0-shard-00-01-erkiu.mongodb.net:27017,cluster0-shard-00-02-erkiu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379"
-};
