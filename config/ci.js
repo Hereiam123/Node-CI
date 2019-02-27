c1e3955fd8c9c78dbd03fa69b0e8774c8132b50b
hereiam123 <bdemaio1@gmail.com>
2019-02-26 23:23:15 -0500

    Travis config

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "668442515260-atdbgi0t8q0pgq1tqs2p5t85qkiic7jp.apps.googleusercontent.com",
+  googleClientSecret: "g6XMYP0O1IBkZ8XGe-U0UyML",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379"
+};
